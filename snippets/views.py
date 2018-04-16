59b1c88cc5e48e40a071ac16217690e125b6472d
sanfutou <723478279@qq.com>
2018-04-16 22:40:43 +0800

    tutorial6-1

--- snippets/views.py
+++ snippets/views.py
@@ -14,6 +14,38 @@ from rest_framework.reverse import reverse
 
 from rest_framework import renderers
 
+from rest_framework import viewsets
+
+from rest_framework.decorators import action
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 class SnippetHighlight(generics.GenericAPIView):
     queryset = Snippet.objects.all()
